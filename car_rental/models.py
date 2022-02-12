3f12d38e941db471810f4e27b3394f587f90f8af
Azar <azarzahra1381@gmail.com>
2022-02-12 13:02:31 +0330

    created 4 models migrated the app

--- car_rental/models.py
+++ car_rental/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 
+class User(models.Model):
+    username = models.CharField(max_length=20)
+    password = models.CharField(max_length=20)
+    credit = models.IntegerField()
+
+
+class Owner(User):
+    pass
+
+
+class Renter(User):
+    pass
+
+
+class Car(models.Model):
+    type = models.CharField(max_length=50)
+    plate = models.CharField(max_length=8)
+    renter = models.ForeignKey(Renter, null=True, default=None, on_delete=models.SET_NULL)
+    owner = models.ForeignKey(Owner, null=False, on_delete=models.CASCADE)
+
+
+
+
+
